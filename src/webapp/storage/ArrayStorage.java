e932dd810257e954a0fc48f2e4c727e4c761c017
Kistina Gonchar <kristina.gonchar.2013@gmail.com>
2021-06-08 23:42:48 +0300

    added first classes

---
+++ src/webapp/storage/ArrayStorage.java
@@ -0,0 +1,29 @@
+package webapp.storage;
+
+
+import webapp.model.Resume;
+
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+
+    public void clear() {
+    }
+
+    public void save(Resume r) {
+    }
+
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    public void delete(String uuid) {
+    }
+
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    public int size() {
+        return size();
+    }
+}
